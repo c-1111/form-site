8445b42660e2999fdb1eb25b33441a96286750a4
c-1111 <cmartinez4086@esdmadrid.es>
2025-09-02 15:22:31 +0200

    Add images to public/webp directory

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -13,7 +13,7 @@ export default function Card({ marca, onClick }: CardProps) {
             onClick={() => onClick(marca)}
             className="flex flex-col items-center w-full p-4 justify-center">
             <div className="card_holder relative w-full">
-                <div className="project_image_overlay absolute inset-0 bg-[#5E5E5E] mix-blend-screen pointer-events-none"></div>
+                {/* <div className="project_image_overlay absolute inset-0 bg-[#5E5E5E] mix-blend-screen pointer-events-none"></div> */}
                 <img src={marca.imagen} alt={marca.titulo} className=" mb-8 px-12 hover:blur-sm cursor-pointer transition duration-75" />
             </div>
             {/* <div className="flex w-full justify-between items-baseline text-sm">
